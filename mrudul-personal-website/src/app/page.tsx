610be28d9a65b6a5c14cd6ffbe18ab269ff18fcc
Mrudul Suresh <mail2mrudul@gmail.com>
2024-03-21 18:58:41 -0400

    install daisyui

--- mrudul-personal-website/src/app/page.tsx
+++ mrudul-personal-website/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Header from "@/components/header/page";
 
 export default function Home() {
   return (
     <div>
-      <div>hiii</div>
+      <div></div>
     </div>
   );
 }
