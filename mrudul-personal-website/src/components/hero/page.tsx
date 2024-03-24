1d230a18a62f74ff2a27e403ee839aaaca671646
Mrudul Suresh <mail2mrudul@gmail.com>
2024-03-24 19:09:38 -0400

    added youtube icon + mobile list button

---
+++ mrudul-personal-website/src/components/hero/page.tsx
@@ -0,0 +1,9 @@
+export default function HeroLanding() {
+  return (
+    <div>
+      <div className="h-[100vh] flex items-center ">
+        <div className="text-4xl font-medium">What's up! Im Mrudul Suresh,</div>
+      </div>
+    </div>
+  );
+}
